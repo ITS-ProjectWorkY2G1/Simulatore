8dde3ab95a9eb6b8c4f13a452c32f4fa7b4183d7
SA_NET/ext-dtarantino <ext-dtarantino@tempestive.com>
2023-06-09 12:26:51 +0200

    Removed db save, added write in azure storage queue

--- Library/Services/SmartWatchService.cs
+++ Library/Services/SmartWatchService.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Azure.Storage.Queues;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Models;
 using Services.Interfaces;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Services.Services
 {
@@ -17,10 +20,18 @@ namespace Services.Services
         }
         public async Task CreateSessionAsync(Guid WatchId, Guid UserId, TimeSpan SessionTime, CancellationToken cancellationToken)
         {
+            JsonConverter serializer;
             PeriodicTimer timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
             WatchContext context = new WatchContext();
             IServiceScope scope = _serviceProvider.CreateScope();
             context = scope.ServiceProvider.GetRequiredService<WatchContext>();
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=cperpwg1;AccountKey=hCqHYjtF4dJdt13i/Ft/w6gAxwINDHdLN7rpeDbh+fdpK5Dhsusm5ppvQpec5Ff/3vtkiCY7lvJH+AStQu6VCg==;EndpointSuffix=core.windows.net";
+
+            QueueClient queueClientSmartWatch = new QueueClient(connectionString, "earth4sport");
+            QueueClient queueClientSessions = new QueueClient(connectionString, "earth4sport-sessions");
+            queueClientSmartWatch.CreateIfNotExists();
+            queueClientSessions.CreateIfNotExists();
+
 
             Random rnd = new Random();
 
@@ -56,31 +67,35 @@ namespace Services.Services
                 }
 
                 smartwatch.Position = $"{newPosition.Latitude}-{newPosition.Longitude}";
-                try
+
+                //Send to queue smartwatch data
+                if (queueClientSmartWatch.Exists())
                 {
-                    await context.AddAsync(smartwatch);
-                    await context.SaveChangesAsync();
+                    Console.WriteLine($"smartwatch data sent in queue: '{queueClientSmartWatch.Name}'");
+                    await queueClientSmartWatch.SendMessageAsync(JsonSerializer.Serialize(smartwatch));
                 }
-                catch (Exception ex)
+                else
                 {
-                    Console.WriteLine(ex.ToString());
+                    Console.WriteLine($"Make sure the Azurite storage emulator running and try again.");
                 }
-                if (SessionTime.TotalSeconds <= 0) break;
             }
-            //Save session informations after api finished
+
+            if (SessionTime.TotalSeconds <= 0)
+            {
+                Console.WriteLine("Session too short");
+                return;
+            }
+
             Random rand = new Random();
             session.PoolLength = (short)rand.Next(4, 51);
             session.PoolLaps = (short)rand.Next(2, 5);
             session.AvgHeartRate = Convert.ToInt32(hearthRates.Average());
             session.SessionDistance = rand.Next(5, 100);
-            try
+
+            if (queueClientSessions.Exists())
             {
-                await context.AddAsync(session);
-                await context.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
+                Console.WriteLine($"Session data sent in queue: '{queueClientSessions.Name}'");
+                await queueClientSessions.SendMessageAsync(JsonSerializer.Serialize(session));
             }
         }
     }
